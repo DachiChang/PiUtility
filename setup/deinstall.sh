2d2236c067e64860392f2718e593a815d2767765
Dachi Chang <femc7488@gmail.com>
2013-11-15 18:58:09 +0800

    use list config to install/deinstall PiUtility

--- setup/deinstall.sh
+++ setup/deinstall.sh
@@ -2,10 +2,20 @@
 
 APACHE_CONFIG_PATH="/usr/local/etc/apache24/Includes"
 
-if [ -f "$APACHE_CONFIG_PATH/PiUtility-cgi.conf" ]; then
-    rm -f $APACHE_CONFIG_PATH/PiUtility-cgi.conf
-    echo "Deinstall PiUtility-cgi.conf restart Apache now ..."
+PiUtility_CONFIG_FILE="PiUtility-cgi.conf"
+RESTART_APACHE="no"
+
+for config_file in $PiUtility_CONFIG_FILE; do
+    if [ -f "$APACHE_CONFIG_PATH/$config_file" ]; then
+        rm -f $APACHE_CONFIG_PATH/$config_file
+        echo "Remove $APACHE_CONFIG_PATH/$config_file"
+        RESTART_APACHE="yes"
+    else
+        echo "$APACHE_CONFIG_PATH/$config_file is not exist"
+    fi
+done
+
+if [ $RESTART_APACHE = "yes" ]; then
+    echo "Deinstall PiUtility finished and restart Apache now ..."
     /usr/local/etc/rc.d/apache24 restart
-else
-    echo "$APACHE_CONFIG_PATH/PiUtility-cgi.conf is not exist"
 fi
--- setup/install.sh
+++ setup/install.sh
@@ -2,10 +2,18 @@
 
 APACHE_CONFIG_PATH="/usr/local/etc/apache24/Includes"
 
-if [ -d "$APACHE_CONFIG_PATH" ]; then
-    cp -f PiUtility-cgi.conf $APACHE_CONFIG_PATH
-    echo "Install PiUtility-cgi.conf to $APACHE_CONFIG_PATH and reset Apache now..."
+PiUtility_CONFIG_FILE="PiUtility-cgi.conf"
+RESTART_APACHE="no"
+
+for config_file in $PiUtility_CONFIG_FILE; do
+    if [ -d "$APACHE_CONFIG_PATH" ]; then
+        cp -f $config_file $APACHE_CONFIG_PATH
+        echo "Install $config_file to $APACHE_CONFIG_PATH/$config_file"
+        RESTART_APACHE="yes"
+    fi
+done
+
+if [ $RESTART_APACHE = "yes" ]; then
+    echo "Install PiUtility finished and restart Apache now ..."
     /usr/local/etc/rc.d/apache24 restart
-else
-    echo "$APACHE_CONFIG_PATH is not exist"
 fi
